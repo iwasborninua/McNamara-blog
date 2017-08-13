0425ae0c854bd6fb2cbe7ea93bb7d54ae4e3800c
Michael McNamara <you@example.com>
2017-08-13 23:50:44 +0300

    added authorization

---
+++ controllers/AuthController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\LoginForm;
+use app\models\User;
+use Yii;
+use yii\web\Controller;
+
+class AuthController extends Controller
+{
+    public function actionLogin()
+    {
+        if (!Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+
+        $model = new LoginForm();
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            return $this->goBack();
+        }
+        return $this->render('/auth/login', [
+            'model' => $model,
+        ]);
+    }
+
+    public function actionLogout()
+    {
+        Yii::$app->user->logout();
+
+        return $this->goHome();
+    }
+
+    public function actionTest()
+    {
+        Yii::$app->user->logout();
+    }
+}
