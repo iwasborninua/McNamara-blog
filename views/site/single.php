8df4233c5273e83619f121301234d0b71d1c071f
Michael McNamara <you@example.com>
2017-08-19 12:53:03 +0300

    fix link's in single post

--- views/site/single.php
+++ views/site/single.php
@@ -6,13 +6,13 @@
             <div class="col-md-8">
                 <article class="post">
                     <div class="post-thumb">
-                        <a href="blog.html"><img src="<?= $article->getImage();?>" alt=""></a>
+                        <a href="<?= Url::toRoute(['site/view', 'id' => $article->id])?>"><img src="<?= $article->getImage();?>" alt=""></a>
                     </div>
                     <div class="post-content">
                         <header class="entry-header text-center text-uppercase">
                             <h6><a href="<?= Url::toRoute(['site/category', 'id' => $article->category->id]);?>"> <?= $article->category->title;?></a></h6>
 
-                            <h1 class="entry-title"><a href="blog.html"><?= $article->title;?></a></h1>
+                            <h1 class="entry-title"><a href="<?= Url::toRoute(['site/view', 'id' => $article->id])?>"><?= $article->title;?></a></h1>
 
 
                         </header>
